88a123052c0e06275a4aab5ed34d87fa82a51825
Xavier Hahn <xavier.hahn@gmail.com>
2019-03-09 10:43:45 +0100

    Added all the columns

--- keyboards/gimly_split/config.h
+++ keyboards/gimly_split/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 #define MATRIX_ROWS 12
-#define MATRIX_COLS 8
+#define MATRIX_COLS 11
 
 /*
  * Keyboard Matrix Assignments
@@ -42,11 +42,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 */
 // #define MATRIX_ROW_PINS { F0, F1, F4, F5, F6, F7 } // Left
-// #define MATRIX_COL_PINS { C6, D4, D6, B4, B5, B6, D7, B0 }//, D1, D2, D3 } // Left
-// #define UNUSED_PINS { B0 }//, D1, D2, D3 }
+// #define MATRIX_COL_PINS { C6, D4, D6, B4, B5, B6, D7, B0, B1, B2, B3 } // Left
+// #define UNUSED_PINS { B0, B1, B2, B3 }
 
 #define MATRIX_ROW_PINS { F0, F1, F4, F5, F6, F7 } // Right
-#define MATRIX_COL_PINS { B0, B1, B7, B3, C6, C7, D4, D7}//, B4, B5, B6 } // Right
+#define MATRIX_COL_PINS { B0, B1, B7, B3, C6, C7, D4, D7, B4, B5, B6 } // Right
 
 /* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
 #define DIODE_DIRECTION COL2ROW
